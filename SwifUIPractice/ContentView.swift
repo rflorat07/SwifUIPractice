59b4e6c8cd7be25cac99c43578ab53d739c5cb87
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-09-09 16:45:47 +0200

    Initial Commit

---
+++ SwifUIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwifUIPractice
+//
+//  Created by Roger Florat Gutierrez on 09/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwifUIPractice/SwifUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIPracticeApp.swift
+//  SwifUIPractice
+//
+//  Created by Roger Florat Gutierrez on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
